bbf275cb9204e30ceae854b7be108196dfcb04ea
Rushi <rushij@bsf.io>
2018-01-16 10:43:34 +0530

    Loads textdomain for the plugin

--- public/includes/assets.class.php
+++ public/includes/assets.class.php
@@ -30,6 +30,8 @@ class wpRemoteSiteSearchAssets
     	add_action('wp_enqueue_scripts', 	array($this,'remote_site_search_scripts'));
 		add_action('wp_enqueue_scripts',	array($this,'remote_site_search_styles'), 99);
 
+		add_action( 'plugins_loaded',       array( $this, 'load_textdomain' ) );
+
 		// include plugin links class
 		require_once( WP_REMOTE_SITE_SEARCH_DIR. '/public/includes/plugin-links-class.php' );
 
@@ -55,7 +57,7 @@ class wpRemoteSiteSearchAssets
 				'no_result' => __('No results found…','wp-remote-site-search'),
 				'we_found' => __('We found','wp-remote-site-search'),
 				'found_msg' => __('articles that may help:','wp-remote-site-search'),
-				'end_point_error' => __('The remote site should have WordPress 4.7 or higher or Rest API (v2) plugin installed to get the search results.')
+				'end_point_error' => __('The remote site should have WordPress 4.7 or higher or Rest API (v2) plugin installed to get the search results.', 'wp-remote-site-search')
 			);
 			wp_localize_script( 'rs-script', 'rs_search_msg', $messages_array );
 
@@ -73,6 +75,15 @@ class wpRemoteSiteSearchAssets
 			// wp remote_site_search style
 			wp_enqueue_style('rs-style', WP_REMOTE_SITE_SEARCH_URL.'/public/assets/css/style.css', WP_REMOTE_SITE_SEARCH_VERSION );
 
+	}
+
+	/**
+	 * Loads textdomain for the plugin.
+	 *
+	 * @since 1.0.1
+	 */
+	function load_textdomain() {
+		load_plugin_textdomain( 'wp-remote-site-search' );
 	}
 }
 $remote_site_search_assets = wpRemoteSiteSearchAssets::instance();
